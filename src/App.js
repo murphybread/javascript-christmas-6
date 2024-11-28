5f59a0aed5bfe3b29222aae7f1d60b5a00e6173b
murphybread <nargene@gmail.com>
2024-11-28 16:52:15 +0900

    feat: dispaly ordered menus with count

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import InputView from "./InputView.js";
+import OutputView from "./OutputView.js";
 
 class App {
 
   async run() {
     const input = await InputView.readDate();
     const menusArray = await InputView.readMenu();
-    
+    OutputView.printMenu(menusArray);
+
+
+
   }
 }
 
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,15 @@
-export default OutputView = {
-    printMenu() {
+import {Console} from "@woowacourse/mission-utils";
+
+const OutputView = {
+    printMenu(menusArray) {
         Console.print("<주문 메뉴>");
-        // ...
+        menusArray.forEach((menu) => {
+            let menuName = menu.split("-")[0];
+            let menuCount = menu.split("-")[1];
+            Console.print(`${menuName} ${menuCount}개`);
+        });
     }
     // ...
 }
+
+export default OutputView;
